dc280fd0714001170b181528ce80fa862fe54e14
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-07-29 20:06:31 +0200

    Show score sum

--- src/main/kotlin/org/wdfeer/kards/qt/widget/FieldWrapper.kt
+++ src/main/kotlin/org/wdfeer/kards/qt/widget/FieldWrapper.kt
@@ -1,14 +1,23 @@
 package org.wdfeer.kards.qt.widget
 
 import io.qt.widgets.QHBoxLayout
+import io.qt.widgets.QLabel
 import org.wdfeer.kards.common.card.Card
 import org.wdfeer.kards.qt.util.WrappedBoxLayout
 
 class FieldWrapper(cards: List<Card>) : WrappedBoxLayout<QHBoxLayout>(QHBoxLayout()) {
     init {
+        layout.addStretch()
+
         cards.forEach {
             layout.addWidget(CardWidget(it))
         }
+
+        layout.addStretch()
+
+        layout.addWidget(QLabel("${cards.sumOf { it.score }} \uDB81\uDCCF").apply {
+            styleSheet = "border: none;"
+        })
 
         widget.styleSheet = "border: 1px solid black;"
     }
