2678b193e0c892a3864f3ea44741b7c5fe47540f
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-07-29 09:14:35 +0200

    Refactor further

--- src/main/kotlin/org/wdfeer/kards/qt/GameWidget.kt
+++ src/main/kotlin/org/wdfeer/kards/qt/GameWidget.kt
@@ -4,11 +4,10 @@ import io.qt.core.Qt
 import io.qt.gui.QKeyEvent
 import io.qt.widgets.*
 import org.wdfeer.kards.common.client.ClientState
-import org.wdfeer.kards.qt.util.QSpacerItem
-import org.wdfeer.kards.qt.util.WrappedBoxLayout
 
 abstract class GameWidget(private val state: ClientState) : QWidget() {
-    private val mainLayout = QVBoxLayout(this)
+    private val mainLayout = createLayout()
+    private fun createLayout(): QVBoxLayout = QVBoxLayout(this)
 
     init {
         createWidgets().forEach { mainLayout.addWidget(it) }
@@ -16,8 +15,10 @@ abstract class GameWidget(private val state: ClientState) : QWidget() {
     }
 
     private fun createWidgets(): List<QWidget> = mutableListOf(
-        PlayerWidget("Opponent", state.opponentCardCount)
-    ) + initCardRows().map { it.widget } + PlayerWidget("You", state.myCards.count())
+        PlayerWidget("Opponent", state.opponentCardCount),
+        RowsWidget(state),
+        PlayerWidget("You", state.myCards.count())
+    )
 
     private fun updateCards() {
         children().forEach { mainLayout.removeWidget(if (it is QWidget) it else return@forEach) }
@@ -36,38 +37,5 @@ abstract class GameWidget(private val state: ClientState) : QWidget() {
             Qt.Key.Key_3.value() -> return 3
             else -> null
         }
-    }
-
-    private fun initCardRows(): List<WrappedBoxLayout<QHBoxLayout>> {
-        val rows: List<WrappedBoxLayout<QHBoxLayout>> = buildList { repeat(3) {
-            add(WrappedBoxLayout(QHBoxLayout()).apply {
-                widget.styleSheet = "border: 1px solid black;"
-            })
-        } }
-        initFields(rows.take(2).map { it.layout })
-        initHand(rows.last().layout)
-        return rows
-    }
-
-    private fun initFields(fields: List<QHBoxLayout>) {
-        state.fields.forEachIndexed { i, f ->
-            f.forEach {
-                fields[i].addWidget(CardWidget(it))
-                fields[i].objectName = "field$i"
-            }
-        }
-    }
-
-    private fun initHand(hand: QHBoxLayout) {
-        addSpacer(hand)
-        state.myCards.forEach {
-            hand.addWidget(CardWidget(it))
-        }
-        hand.objectName = "myHand"
-        addSpacer(hand)
-    }
-
-    private fun addSpacer(row: QHBoxLayout) {
-        row.addSpacerItem(QSpacerItem.ExpMin)
     }
 }
---
+++ src/main/kotlin/org/wdfeer/kards/qt/RowsWidget.kt
@@ -0,0 +1,49 @@
+package org.wdfeer.kards.qt
+
+import io.qt.widgets.QHBoxLayout
+import io.qt.widgets.QVBoxLayout
+import io.qt.widgets.QWidget
+import org.wdfeer.kards.common.client.ClientState
+import org.wdfeer.kards.qt.util.QSpacerItem
+import org.wdfeer.kards.qt.util.WrappedBoxLayout
+
+class RowsWidget(private val state: ClientState) : QWidget() {
+    init {
+        val layout = QVBoxLayout()
+        createCardRows().forEach { layout.addWidget(it.widget) }
+        setLayout(layout)
+    }
+
+    private fun createCardRows(): List<WrappedBoxLayout<QHBoxLayout>> {
+        val rows: List<WrappedBoxLayout<QHBoxLayout>> = buildList { repeat(3) {
+            add(WrappedBoxLayout(QHBoxLayout()).apply {
+                widget.styleSheet = "border: 1px solid black;"
+            })
+        } }
+        initFields(rows.take(2).map { it.layout })
+        initHand(rows.last().layout)
+        return rows
+    }
+
+    private fun initFields(fields: List<QHBoxLayout>) {
+        state.fields.forEachIndexed { i, f ->
+            f.forEach {
+                fields[i].addWidget(CardWidget(it))
+                fields[i].objectName = "field$i"
+            }
+        }
+    }
+
+    private fun initHand(hand: QHBoxLayout) {
+        addSpacer(hand)
+        state.myCards.forEach {
+            hand.addWidget(CardWidget(it))
+        }
+        hand.objectName = "myHand"
+        addSpacer(hand)
+    }
+
+    private fun addSpacer(row: QHBoxLayout) {
+        row.addSpacerItem(QSpacerItem.ExpMin)
+    }
+}
