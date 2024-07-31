4825f302aa76173a2f0a6ad84b9b6d2884d8310c
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-07-31 11:08:39 +0200

    Use the stable AIs for now

--- src/main/kotlin/org/wdfeer/kards/common/server/ai/AI.kt
+++ src/main/kotlin/org/wdfeer/kards/common/server/ai/AI.kt
@@ -3,7 +3,7 @@ package org.wdfeer.kards.common.server.ai
 import org.wdfeer.kards.common.server.ServerState
 
 object AI {
-    private val algorithm: AiAlgorithm = Recursive()
+    private val algorithm: AiAlgorithm = listOf(Random(), Greedy()).random()
 
     fun chooseCardToPlay(state: ServerState, player: Int): Int {
         val values = state.hands[player].map { algorithm.evaluate(state, it, player) }
--- src/main/kotlin/org/wdfeer/kards/common/server/ai/Random.kt
+++ src/main/kotlin/org/wdfeer/kards/common/server/ai/Random.kt
@@ -3,6 +3,6 @@ package org.wdfeer.kards.common.server.ai
 import org.wdfeer.kards.common.card.Card
 import org.wdfeer.kards.common.server.ServerState
 
-class Zero : AiAlgorithm {
+class Random : AiAlgorithm {
     override fun evaluate(presentState: ServerState, cardToPlay: Card, player: Int): Int = 0
 }
