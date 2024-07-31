bf6dd17c88d73746cb96c0c2cc93471035576dfa
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-07-31 11:10:49 +0200

    Randomize hands again

--- src/main/kotlin/org/wdfeer/kards/common/server/ServerState.kt
+++ src/main/kotlin/org/wdfeer/kards/common/server/ServerState.kt
@@ -13,9 +13,7 @@ import kotlin.random.Random
 
 data class ServerState(
     val fields: List<MutableList<MutableCard>> = listOf(mutableListOf(), mutableListOf()),
-
-    // TODO: DEBUG
-    val hands: List<MutableList<CardType>> = listOf(CardType.entries.toMutableList(), CardType.entries.toMutableList())
+    val hands: List<MutableList<CardType>> = listOf(Hand.getRandom(7).toMutableList(), Hand.getRandom(7).toMutableList())
 ) {
     private var playing = Random.nextInt(2)
     private fun flipWhoPlaying() { playing = 1 - playing }
